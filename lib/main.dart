b2ca5db7fee9bc9e332136f476c7ee6c0ea10774
Shahriar Ahmed Shovo <117426913+shovo404@users.noreply.github.com>
2025-02-27 14:18:16 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -11,10 +12,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  // Track the theme mode (light or dark)
   bool _isDarkMode = false;
 
-  // Toggle theme mode between light and dark
   void _toggleTheme() {
     setState(() {
       _isDarkMode = !_isDarkMode;
@@ -26,48 +25,37 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       themeMode: _isDarkMode ? ThemeMode.dark : ThemeMode.light,
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.blue),
-      ),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.black),
-      ),
-      home: FullScreenImages(
-          toggleTheme: _toggleTheme), // Pass toggle function to child
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      home: MainScreen(toggleTheme: _toggleTheme),
     );
   }
 }
 
-class FullScreenImages extends StatefulWidget {
-  final VoidCallback toggleTheme; // Function passed from MyApp
+class MainScreen extends StatefulWidget {
+  final VoidCallback toggleTheme;
 
-  FullScreenImages({required this.toggleTheme});
+  MainScreen({required this.toggleTheme});
 
   @override
-  _FullScreenImagesState createState() => _FullScreenImagesState();
+  _MainScreenState createState() => _MainScreenState();
 }
 
-class _FullScreenImagesState extends State<FullScreenImages> {
+class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
+  String _selectedCategory = "nature";  // Default category
 
-  final List<String> imageUrls = [
-    'https://images.unsplash.com/photo-1739609579483-00b49437cc45?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1735317461815-1a0ba64e9a56?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://plus.unsplash.com/premium_photo-1717529138029-5b049119cfb1?q=80&w=1994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1735276680702-c008c8035c18?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1735424080768-8730f9c8a0e9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1726317986176-382bffd78c99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
-  final List<Widget> _screens = [
-    WelcomePage(), // Home screen (Welcome page)
-    FullScreenImagesPage(), // Image screen
-    SettingsPage(), // Settings screen (placeholder)
-  ];
+  void _onCategorySelected(String category) {
+    setState(() {
+      _selectedCategory = category;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,23 +65,16 @@ class _FullScreenImagesState extends State<FullScreenImages> {
         actions: [
           IconButton(
             icon: Icon(Icons.brightness_6),
-            onPressed: widget.toggleTheme, // Toggle theme on button press
+            onPressed: widget.toggleTheme,
           ),
         ],
       ),
       drawer: Drawer(
-        elevation: 16,
         child: ListView(
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30),
-                  bottomRight: Radius.circular(30),
-                ),
-              ),
+              decoration: BoxDecoration(color: Colors.blue),
               child: Column(
                 children: [
                   CircleAvatar(
@@ -110,22 +91,18 @@ class _FullScreenImagesState extends State<FullScreenImages> {
                 ],
               ),
             ),
-            _buildDrawerItem('Food', () {
-              _onItemClick('Food');
-            }),
-            _buildDrawerItem('Nature', () {
-              _onItemClick('Nature');
-            }),
-            _buildDrawerItem('Photography', () {
-              _onItemClick('Photography');
-            }),
+            _buildDrawerItem('Food', 'food'),
+            _buildDrawerItem('Nature', 'nature'),
+            _buildDrawerItem('Photography', 'photography'),
           ],
         ),
       ),
-      body: _screens[_selectedIndex],
+      body: _selectedIndex == 0
+          ? WelcomePage()
+          : FullScreenImagesPage(category: _selectedCategory),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
-        onTap: _onNavigationItemTap,
+        onTap: _onItemTapped,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
           BottomNavigationBarItem(icon: Icon(Icons.image), label: 'Images'),
@@ -134,95 +111,140 @@ class _FullScreenImagesState extends State<FullScreenImages> {
     );
   }
 
-  Widget _buildDrawerItem(String title, VoidCallback onTap) {
-    return InkWell(
-      onTap: onTap,
-      child: ListTile(
-        title: Text(title),
-        contentPadding: EdgeInsets.symmetric(horizontal: 20),
-      ),
+  Widget _buildDrawerItem(String title, String category) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.pop(context);
+        _onCategorySelected(category);
+      },
     );
   }
-
-  void _onItemClick(String item) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text('You clicked $item')));
-  }
-
-  void _onNavigationItemTap(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 }
 
-// Welcome page with typing animation
-class WelcomePage extends StatefulWidget {
-  @override
-  _WelcomePageState createState() => _WelcomePageState();
-}
-
-class _WelcomePageState extends State<WelcomePage> {
-  String _displayText = '';
-  final String _fullText = 'Welcome 🎞 Store Dev. By SHOVO🏴‍☠️';
-
-  @override
-  void initState() {
-    super.initState();
-    _startTypingAnimation();
-  }
-
-  void _startTypingAnimation() async {
-    for (int i = 0; i < _fullText.length; i++) {
-      await Future.delayed(Duration(milliseconds: 100));
-      setState(() {
-        _displayText = _fullText.substring(0, i + 1);
-      });
-    }
-  }
-
+class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        _displayText,
+        'Developed by Shovo',
         style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
       ),
     );
   }
 }
 
-// Image screen widget
-class FullScreenImagesPage extends StatelessWidget {
-  final List<String> imageUrls = [
-    'https://images.unsplash.com/photo-1735317461815-1a0ba64e9a56?q=80&w=2127&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://plus.unsplash.com/premium_photo-1717529138029-5b049119cfb1?q=80&w=1994&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1735276680702-c008c8035c18?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1735424080768-8730f9c8a0e9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1726317986176-382bffd78c99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1734640548068-1cfe6cb6b96b?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  ];
+// Fetch Images from Unsplash
+class FullScreenImagesPage extends StatefulWidget {
+  final String category;
+
+  FullScreenImagesPage({required this.category});
+
+  @override
+  _FullScreenImagesPageState createState() => _FullScreenImagesPageState();
+}
+
+class _FullScreenImagesPageState extends State<FullScreenImagesPage> {
+  List<String> imageUrls = [];
+  final String unsplashApiKey =
+      "gpc1EnKt1pI5SEB6uOE2WNhMhhsGfW0fwI5NT9AuohA";
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchImages(widget.category);
+  }
+
+  Future<void> _fetchImages(String query) async {
+    final url = Uri.parse(
+        "https://api.unsplash.com/search/photos?query=$query&per_page=10&client_id=$unsplashApiKey");
+
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      // Fetch the image results and shuffle them for randomness
+      List<String> fetchedImageUrls = List<String>.from(
+          data["results"].map((image) => image["urls"]["regular"]));
+      fetchedImageUrls.shuffle();  // Shuffle the list to randomize the images
+
+      setState(() {
+        imageUrls = fetchedImageUrls;
+      });
+    } else {
+      print("Failed to load images");
+    }
+  }
+
+  @override
+  void didUpdateWidget(FullScreenImagesPage oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.category != widget.category) {
+      _fetchImages(widget.category); // Fetch new images when the category changes
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: imageUrls.map((url) {
-          return Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: Image.network(url, fit: BoxFit.cover),
+    return imageUrls.isEmpty
+        ? Center(child: CircularProgressIndicator())
+        : GridView.builder(
+            padding: EdgeInsets.all(10),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+            ),
+            itemCount: imageUrls.length,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) =>
+                          ImageDetailScreen(imageUrl: imageUrls[index]),
+                    ),
+                  );
+                },
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.network(imageUrls[index], fit: BoxFit.cover),
+                ),
+              );
+            },
           );
-        }).toList(),
-      ),
-    );
   }
 }
 
-// Settings screen widget (placeholder)
-class SettingsPage extends StatelessWidget {
+// Image Detail Page with Full-Screen View
+class ImageDetailScreen extends StatelessWidget {
+  final String imageUrl;
+
+  ImageDetailScreen({required this.imageUrl});
+
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Settings Page', style: TextStyle(fontSize: 20)));
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      backgroundColor: Colors.black,
+      body: Center(
+        child: InteractiveViewer(
+          panEnabled: false,
+          boundaryMargin: EdgeInsets.all(20),
+          minScale: 0.5,
+          maxScale: 2.5,
+          child: Image.network(imageUrl, fit: BoxFit.contain),
+        ),
+      ),
+    );
   }
 }
